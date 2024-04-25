39c8ab97b6bc017db588256d5a2021b0b1d0bf53
Faisal Mohammed <m.faisal244@gmail.com>
2024-04-25 01:18:11 +0100

    Join meeting functionality complete, bugfixes

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -86,12 +86,14 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
 								: "/icons/recordings.svg"
 						}
 						title={
-							(meeting as Call).state?.custom?.description ||
-							(meeting as CallRecording).filename?.substring(
+							(
+								meeting as Call
+							).state?.custom?.description?.substring(0, 26) ||
+							(meeting as CallRecording)?.filename?.substring(
 								0,
 								20
 							) ||
-							"No Description"
+							"Personal Meeting"
 						}
 						date={
 							(
